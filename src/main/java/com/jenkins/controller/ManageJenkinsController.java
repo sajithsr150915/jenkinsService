3a9f7ed875605f296b3e69d5bc25f22b921d00de
Sheena <Sheena>
2020-04-10 19:14:59 +0530

    Added more APIs and corresponding service methods

--- src/main/java/com/jenkins/controller/ManageJenkinsController.java
+++ src/main/java/com/jenkins/controller/ManageJenkinsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jenkins.model.BuildInfo;
 import com.jenkins.model.JobDetails;
+import com.jenkins.model.PipelineNode;
 import com.jenkins.service.ManageJenkinsService;
 
 
@@ -48,6 +49,74 @@ public class ManageJenkinsController {
 		JobDetails jobDetails = manageJenkinsService.detailsOfJob(jobName);
 
 		return jobDetails;
+	}
+	
+	/**
+	 * Method for getting the details of the last build of a job
+	 * by passing the jobName
+	 * @param jobName
+	 * @return BuildInfo
+	 */
+	
+	@GetMapping("/latestBuildOfJob/{jobName}")
+	public BuildInfo latestBuildOfJob(@PathVariable("jobName") String jobName) {
+
+		BuildInfo buildInfo = manageJenkinsService.latestBuildOfJob(jobName);
+		return buildInfo;
+	}
+	
+	/**
+	 * Method for getting the details of the last Stable build of a Job
+	 * passing the jobName
+	 * @param jobName
+	 * @return BuildInfo 
+	 */
+	@GetMapping("/lastStableBuild/{jobName}")
+	public BuildInfo lastStableBuild(@PathVariable("jobName") String jobName) {
+
+		BuildInfo buildInfo = manageJenkinsService.lastStableBuild(jobName);
+		return buildInfo;
+	}
+	
+	/**
+	 * Method for getting the details of the last successful build of a job
+	 * by passing the jobName
+	 * @param jobName
+	 * @return BuildInfo
+	 */
+	@GetMapping("/lastSuccessfulBuild/{jobName}")
+	public BuildInfo lastSuccessfulBuild(@PathVariable("jobName") String jobName) {
+
+		BuildInfo buildInfo = manageJenkinsService.lastSuccessfulBuild(jobName);
+		return buildInfo;
+	}
+	
+	/**
+	 * Method for getting the details of the last failed build of a job
+	 * by passing the jobName
+	 * @param jobName
+	 * @return BuildInfo
+	 */
+	@GetMapping("/lastFailedBuild/{jobName}")
+	public BuildInfo lastFailedBuild(@PathVariable("jobName") String jobName) {
+
+		BuildInfo buildInfo = manageJenkinsService.lastFailedBuild(jobName);
+		return buildInfo;
+	}
+	
+	/**
+	 * Method for getting the details of a stage/node of a pipeline by passing the
+	 * jobName/Pipeline name, build number and nodeId
+	 * 
+	 * @param jobName
+	 * @return PipelineNode
+	 */
+	@GetMapping("/pipelineNode/{jobName}/{buildNo}/{nodeId}")
+	public PipelineNode pipelineNode(@PathVariable("jobName") String jobName, @PathVariable("buildNo") String buildNo,
+			@PathVariable("nodeId") String nodeId) {
+
+		PipelineNode details = manageJenkinsService.pipelineNode(jobName, buildNo, nodeId);
+		return details;
 	}
 	
 	
--- src/main/java/com/jenkins/service/ManageJenkinsService.java
+++ src/main/java/com/jenkins/service/ManageJenkinsService.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.jenkins.model.BuildInfo;
 import com.jenkins.model.JobDetails;
+import com.jenkins.model.PipelineNode;
 
 @Service
 @Component
@@ -22,83 +23,77 @@ public class ManageJenkinsService {
 
 	@Autowired
 	private JenkinsClient jenkinsClient;
-	
-	
-	private static final  String  jenkinsJobURI="/api/json";
-	
-	
-	
-	 Logger logger = LoggerFactory.getLogger(ManageJenkinsService.class);
 
+	private static final String jenkinsJobURI = "/api/json";
+
+	Logger logger = LoggerFactory.getLogger(ManageJenkinsService.class);
+
+	private final RestTemplate restTemplate;
+	@Value("${jenkinsURL}")
+	private String url;
+
+	private static String username = "admin";
+	private static String password = "admin";
 	
+
+	public ManageJenkinsService(RestTemplateBuilder restTemplateBuilder) {
+		this.restTemplate = restTemplateBuilder
+				.basicAuthentication(username, password)
+				.build();
+	}
+
+	/**
+	 * Sample code for getting the last completed build of a job Used the
+	 * RestTemplate way to fetch the details from jenkins as part of a trial method
+	 * for reducing the code.
+	 */
+	public BuildInfo lastCompletedBuild(String jobName) {
+
+		String jenkinsUrl = this.url + "/job/{jobName}/lastCompletedBuild/api/json";
+		return this.restTemplate.getForObject(jenkinsUrl, BuildInfo.class, jobName);
+	}
+
+	public JobDetails detailsOfJob(String jobName) {
+
+		String jenkinsUrl = this.url + "/job/{jobName}/api/json?tree=builds[number,status,timestamp,id,result]";
+		return this.restTemplate.getForObject(jenkinsUrl, JobDetails.class, jobName);
+
+	}
+
+	public String overallLoad() {
+
+		String jenkinsUrl = this.url + "/overallLoad/api/json";
+		return this.restTemplate.getForObject(jenkinsUrl, String.class);
+	}
 	
+	public BuildInfo latestBuildOfJob(String jobName) {
 		
-		
-		public String lastSuccessfulBuild(String jobName) {
-
-			try {
-				String URI = "/job/jobName/lastSuccessfulBuild/api/json";
-				URI=URI.replace("jobName", jobName);
-				return jenkinsClient.jenkinsGetAPI(URI);
-			} catch (AuthenticationException e) {
-				logger.error("AuthenticationException error" + e);
-			} catch (ClientProtocolException e) {
-				logger.error("ClientProtocolException error" + e);
-
-			} catch (IOException e) {
-				logger.error("IOException error" + e);
-
-			}
-			return null;
-		}
-				
-		
-		
-		
-		/**
-		 * Sample code for getting the last completed build of a job
-		 * Used the RestTemplate way to fetch the details from jenkins
-		 * as part of a trial method for reducing the code.
-		 */
-		
-		private final RestTemplate restTemplate;		
-		@Value("${jenkinsURL}")
-		private String url;
-		
-		private static String username = "admin";
-		private static String password = "admin";
-		
-		public ManageJenkinsService(RestTemplateBuilder restTemplateBuilder) {
-			this.restTemplate = restTemplateBuilder
-					.basicAuthentication(username, password)
-					.build();
-		}
-			
-		public BuildInfo lastCompletedBuild(String jobName) {
-			
-			String jenkinsUrl = this.url + "/job/{jobName}/lastCompletedBuild/api/json";
-			return this.restTemplate.getForObject(jenkinsUrl, BuildInfo.class, jobName);
-		}
-		
-		
-		public JobDetails detailsOfJob(String jobName) {
-			
-			String jenkinsUrl = this.url + "/job/{jobName}/api/json?tree=builds[number,status,timestamp,id,result]";
-			return this.restTemplate.getForObject(jenkinsUrl, JobDetails.class, jobName);
-
-			
-		}
-
-		
-			public String overallLoad() {
-			
-			String jenkinsUrl = this.url + "/overallLoad/api/json";
-			return this.restTemplate.getForObject(jenkinsUrl, String.class);
-		}
-		
-			
-			
-			
-		
+		String jenkinsUrl = this.url + "/job/{jobName}/lastBuild/api/json";
+		return this.restTemplate.getForObject(jenkinsUrl, BuildInfo.class, jobName);
+	}
 	
+	public BuildInfo lastStableBuild(String jobName) {
+		
+		String jenkinsUrl = this.url + "/job/{jobName}/lastStableBuild/api/json";
+		return this.restTemplate.getForObject(jenkinsUrl, BuildInfo.class, jobName);
+	}
+	
+	public BuildInfo lastSuccessfulBuild(String jobName) {
+		
+		String jenkinsUrl = this.url + "/job/{jobName}/lastSuccessfulBuild/api/json";
+		return this.restTemplate.getForObject(jenkinsUrl, BuildInfo.class, jobName);
+	}
+	
+	public BuildInfo lastFailedBuild(String jobName) {
+		
+		String jenkinsUrl = this.url + "/job/{jobName}/lastFailedBuild/api/json";
+		return this.restTemplate.getForObject(jenkinsUrl, BuildInfo.class, jobName);
+	}
+	
+	public PipelineNode pipelineNode(String jobName, String buildNo, String nodeId) {
+
+		String jenkinsUrl = this.url + "/job/{jobName}/{buildNo}/execution/node/{nodeId}/wfapi/describe";
+		return this.restTemplate.getForObject(jenkinsUrl, PipelineNode.class, jobName, buildNo, nodeId);
+	}
+
 }
